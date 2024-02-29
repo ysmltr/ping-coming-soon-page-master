585232aeffee1b7e2a0f333563276b96b2cd3c5d
ysmltr <yesimltr1@gmail.com>
2024-02-29 16:03:59 +0300

    started js file

---
+++ src/js/notify.js
@@ -0,0 +1,12 @@
+/* notify part*/
+
+const email = document.getElementById("e-mail")
+const btn =  document.getElementById("submit")
+
+const regexEmail = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
+
+function validEmail(i) {
+    
+    
+}
